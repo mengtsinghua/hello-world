f17ad1142a808b173f3ff1bf461f4d286f5b4d90
MickeyMeng <mengtsinghua@hotmail.com>
2018-07-01 20:47:58 +0800

    clarify the position of the HelloWorld

--- src/com/bianjiebao/HelloWorld.java
+++ src/com/bianjiebao/HelloWorld.java
@@ -2,6 +2,7 @@ package com.bianjiebao;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Bingo!");
+        System.out.println("Bingo! This is for validating the integration of IDEA and git");
+        System.out.println("But it may change its purpose some time later");
     }
 }
